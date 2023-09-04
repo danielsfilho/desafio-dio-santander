c350e00c94d42c68dd6f9cbf0458f685c0966b78
Daniel S. Filho <danielbrumdasilva@gmail.com>
2023-09-04 01:02:10 -0300

    feat: add program to create bank account from terminal

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,14 +1,35 @@
 
+import entities.ContaBanco;
+
+import java.util.Locale;
+import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) {
         // TODO: Knowing and importing the Scanner class
-
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
 
         // Display messages to our user
+        System.out.print("Por favor, digite o número da conta: ");
+        Integer numConta = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("Por favor, digite o número da Agência: ");
+        String numAgencia = sc.nextLine();
+
+        System.out.print("Por favor, digite o nome do cliente: ");
+        String cliente = sc.nextLine();
+
+        System.out.print("Por favor, digite o saldo: ");
+        Double saldo = sc.nextDouble();
 
         // Get the values entered in the terminal
+        ContaBanco conta = new ContaBanco(numConta, numAgencia, cliente, saldo);
 
         // Display account created message
+        System.out.println(conta);
+
+        sc.close();
     }
 }
--- src/entities/ContaBanco.java
+++ src/entities/ContaBanco.java
@@ -1,5 +1,7 @@
 package entities;
 
+import java.text.DecimalFormat;
+
 public class ContaBanco {
     private Integer numero;
     private String agencia;
@@ -45,5 +47,22 @@ public class ContaBanco {
 
     public void setSaldo(Double saldo) {
         this.saldo = saldo;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        DecimalFormat decimal = new DecimalFormat("0.00");
+
+        sb.append("Olá ");
+        sb.append(this.getNomeCliente());
+        sb.append(", obrigado por criar uma conta em nosso banco, sua agência é ");
+        sb.append(this.getAgencia());
+        sb.append(", conta ");
+        sb.append(this.getNumero());
+        sb.append(" e seu saldo ");
+        sb.append(decimal.format(this.getSaldo()));
+        sb.append(" já está disponível!");
+        return sb.toString();
     }
 }
