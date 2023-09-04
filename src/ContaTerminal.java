652a8a58b4549c96cacf22fe95386d97c2223d35
Daniel S. Filho <danielbrumdasilva@gmail.com>
2023-09-03 21:40:46 -0300

    feat: add the ContaBanco class

---
+++ src/ContaTerminal.java
@@ -0,0 +1,14 @@
+
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        // TODO: Knowing and importing the Scanner class
+
+
+        // Display messages to our user
+
+        // Get the values entered in the terminal
+
+        // Display account created message
+    }
+}
---
+++ src/entities/ContaBanco.java
@@ -0,0 +1,49 @@
+package entities;
+
+public class ContaBanco {
+    private Integer numero;
+    private String agencia;
+    private String nomeCliente;
+    private Double saldo;
+
+    public ContaBanco() {}
+
+    public ContaBanco(Integer numero, String agencia, String nomeCliente, Double saldo) {
+        this.setNumero(numero);
+        this.setAgencia(agencia);
+        this.setNomeCliente(nomeCliente);
+        this.setSaldo(saldo);
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
+    }
+}
